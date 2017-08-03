a79b215d7b1ba7a63e5ff225e5a9b3bc3c64eb8a
negebauer <negebauer@uc.cl>
2017-08-03 15:28:13 -0400

    circle.yml

--- scripts/env_file.sh
+++ scripts/env_file.sh
@@ -1,1 +1,15 @@
-echo "User: $UC_USERNAME"
+f="__tests__/.env.json"
+
+keyVal() {
+  echo "{$1}: {$2}\n" >> $f
+}
+
+echo "{" > $f
+
+keyVal "username" $UC_USERNAME
+# keyVal "password" $UC_PASSWORD
+
+echo "}" >> $f
+
+echo 'file'
+cat $f
