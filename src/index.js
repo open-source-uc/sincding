a9aa2b0e40c14366f5b4c1d07f1f63ecefae133f
negebauer <negebauer@uc.cl>
2017-08-03 10:48:29 -0400

    Lint project

--- src/index.js
+++ src/index.js
@@ -1,46 +1,45 @@
 #!/usr/bin/env node
 
-const prompt = require('prompt')
-const fs = require('fs')
-const os = require('os')
-const updateNotifier = require('update-notifier')
-const pkg = require('../package.json')
-const Session = require('../lib/session')
-const siding = require('../lib/siding')
-const error = require('../lib/error')
-const wait = require('../lib/wait')
-const log = require('./log')
+const prompt = require("prompt")
+const fs = require("fs")
+const os = require("os")
+const updateNotifier = require("update-notifier")
+const pkg = require("../package.json")
+const Session = require("../lib/session")
+const siding = require("../lib/siding")
+const error = require("../lib/error")
+const log = require("./log")
 
 prompt.colors = false
 
 const userDataFolder = `${os.homedir()}/.sincding`
 
-data = (data = {}) => {
-  console.log('Let\'s update your user data')
+const data = (data = {}) => {
+  console.log("Let's update your user data")
   if (data.username) {
     console.log(
-      ' For each prompt the value in () is the current one\n Press enter if you don\'t want to change it'
+      " For each prompt the value in () is the current one\n Press enter if you don't want to change it"
     )
   }
   const schema = {
     properties: {
       username: {
         pattern: /^[a-zA-Z\d]+$/,
-        message: 'Username without @uc',
+        message: "Username without @uc",
         required: true,
-        default: data.username || '',
+        default: data.username || "",
       },
       password: {
         required: true,
         hidden: true,
-        replace: '*',
+        replace: "*",
       },
       path: {
         required: true,
-        default: data.path || '',
+        default: data.path || "",
         conform: path => {
           if (!fs.existsSync(path)) {
-            console.log('The provided path doesn\'t exist')
+            console.log("The provided path doesn't exist")
             return false
           }
           return true
@@ -48,8 +47,8 @@ data = (data = {}) => {
       },
       ignore: {
         pattern: /^[a-zA-Z\d ]+$/,
-        message: 'Course acronyms separated by a space',
-        default: (data.ignore || []).join(' '),
+        message: "Course acronyms separated by a space",
+        default: (data.ignore || []).join(" "),
       },
     },
   }
@@ -59,20 +58,20 @@ data = (data = {}) => {
       return exit()
     }
     const data = Object.assign({}, result, {
-      ignore: result.ignore.split(' ').map(a => a.toUpperCase()),
+      ignore: result.ignore.split(" ").map(a => a.toUpperCase()),
     })
     if (!fs.existsSync(`${userDataFolder}`)) {
       fs.mkdirSync(`${userDataFolder}`)
     }
     const path = `${userDataFolder}/data.json`
     const dataJson = JSON.stringify(data)
-    fs.writeFile(path, dataJson, 'utf8', run)
-    console.log('Updated user data')
+    fs.writeFile(path, dataJson, "utf8", run)
+    console.log("Updated user data")
   }
   prompt.get(schema, saveData)
 }
 
-sync = async data => {
+const sync = async data => {
   const session = new Session(data.username, data.password)
   try {
     const courses = await siding.coursesList(session, data.ignore)
@@ -89,11 +88,11 @@ sync = async data => {
     }))
     log.coursesFiles(courses, downloads)
 
-    console.log('\nCreating missing folders...')
+    console.log("\nCreating missing folders...")
     courses.forEach(course => course.createFolder(data.path))
     downloads.forEach(d => d.folders.forEach(f => f.create(data.path)))
 
-    console.log('\nStarting downloads, this may take a while...')
+    console.log("\nStarting downloads, this may take a while...")
     const files = downloads
       .map(download => download.files)
       .reduce((total, arr) => total.concat(arr), [])
@@ -101,22 +100,24 @@ sync = async data => {
       await promise
       return file.download(data.path)
     }, Promise.resolve())
-    console.log('\nFinished downloading!')
+    console.log("\nFinished downloading!")
   } catch (err) {
-    error(err.message, 'sync')
+    error(err.message, "sync")
   }
 }
 
-loadUserData = () => {
+const loadUserData = () => {
   let userData = null
   try {
     userData = require(`${userDataFolder}/data.json`)
-  } catch (err) {}
+  } catch (err) {
+    console.error("Could't load user data")
+  }
   return userData
 }
 
-run = async () => {
-  console.log('')
+const run = async () => {
+  console.log("")
   // Load user data
   const userData = loadUserData()
   if (!userData) {
@@ -126,28 +127,28 @@ run = async () => {
   // If a command was supplied in the call, execute it
   const command = process.argv[2]
   if (command) {
-    process.argv[2] = ''
+    process.argv[2] = ""
     return runCommand(userData, command)
   }
   // Show available commands
-  console.log('Commands:')
+  console.log("Commands:")
   Object.keys(options).forEach(key => {
     console.log(` - ${key}: ${optionsDescriptions[key]}`)
   })
   // Prompt user for command
   prompt.start()
-  prompt.get(['command',], (err, result) =>
+  prompt.get(["command"], (err, result) =>
     runCommand(userData, (result || {}).command, err)
   )
 }
 
-runCommand = (userData, command, err) => {
+const runCommand = (userData, command, err) => {
   if (err) {
     return options.exit()
   }
   const action = options[command]
   if (!action) {
-    console.log('Not a valid command')
+    console.log("Not a valid command")
     return run()
   }
   // Execute selected command
@@ -155,20 +156,22 @@ runCommand = (userData, command, err) => {
   action(userData)
 }
 
-options = {
+const exit = () => console.log("\nTerminated sincding")
+
+const options = {
   data: data,
   sync: sync,
-  exit: () => console.log('\nTerminated sincding'),
+  exit: exit,
 }
 
-optionsDescriptions = {
-  data: 'Update your user data',
-  sync: 'Download sincding files',
-  exit: 'Exit sincding',
+const optionsDescriptions = {
+  data: "Update your user data",
+  sync: "Download sincding files",
+  exit: "Exit sincding",
 }
 
 const updateCheckInterval = 1000 * 60 * 60
-const notifier = updateNotifier({ pkg, updateCheckInterval, }).notify() // 1 hour
-console.log('Welcome to sincding!')
+updateNotifier({ pkg, updateCheckInterval }).notify() // 1 hour
+console.log("Welcome to sincding!")
 console.log(`Version ${pkg.version}`)
 run()
