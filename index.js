2439d0a3f9672a10bbeae3dd56c43cda3eb84169
negebauer <negebauer@uc.cl>
2017-08-03 11:01:42 -0400

    Missing eslint run

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const course = require('./lib/course'),
-  file = require('./lib/file'),
-  session = require('./lib/session'),
-  folder = require('./lib/folder'),
-  siding = require('./lib/siding')
+const course = require("./lib/course"),
+  file = require("./lib/file"),
+  session = require("./lib/session"),
+  folder = require("./lib/folder"),
+  siding = require("./lib/siding")
 
 module.exports = {
   course,
   file,
   session,
   folder,
-  siding
+  siding,
 }
