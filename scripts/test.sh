c0dae4f0d0a3e5264e138f9571d2f2367a4f3020
negebauer <negebauer@uc.cl>
2017-08-03 20:25:44 -0400

    circle

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,21 +1,24 @@
 if [[ $CIRCLECI ]]
 then
-  echo 'Running test in cli'
-  if [[ $UC_USERNAME && $UC_PASSWORD ]]
-  then
-    echo 'Valid credentials'
-  else
+  if [[ ! $UC_USERNAME || ! $UC_PASSWORD ]]; then
     echo 'ERROR: No valid credentials'
     exit 1
   fi
-  if [[ $CIRCLE_USERNAME == $UC_USERNAME ]]
-  then
-    echo 'Valid user'
-  else
+  if [[ $CIRCLE_USERNAME != $UC_USERNAME ]]; then
     echo 'ERROR: Not valid user'
     exit 1
   fi
-  ./scripts/env_file.sh
+
+  f="./__tests__/.env.json"
+  keyVal() {
+    if [[ $3 == 0 ]]; then end=""; else end=","; fi
+    echo "\"${1}\": \"${2}\"${end}" >> $f
+  }
+  echo "{" > $f
+  keyVal "username" $UC_USERNAME
+  keyVal "password" $UC_PASSWORD 0
+  echo "}" >> $f
+
   jest
 else
   jest --notify
